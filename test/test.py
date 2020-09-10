7202c1b0c237a4bfbcba1feaa0d45537c073bf07
katomasahiro10 <katomasahiro10@gmail.com>
2020-09-10 22:42:22 +0900

    多分ダメになる

--- test/test.py
+++ test/test.py
@@ -1,9 +1,11 @@
 from hello import hello
 
+
 def test_hello_default(capsys):
     hello()
     out, err = capsys.readouterr()
     assert out == "Hello, world.\n"
+
 
 def test_hello_with_name(capsys):
     hello("サザエ")
